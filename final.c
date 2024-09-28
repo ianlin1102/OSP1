0e9b8a9bd8b8e2fa6b36a6932ead8f4fefcc0f18
ianlin1102 <73145204+ianlin1102@users.noreply.github.com>
2024-09-27 21:49:02 -0500

    Update final.c

--- final.c
+++ final.c
@@ -6,258 +6,181 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
-#define MAX_COMMAND 100
-#define MAX_ARGS 100
-#define MAX_PATH 100
-#define MAX_PATH_LENGTH 1024
-const char error_message[30] = "An error has occurred\n";
-/*
-paths: all the potential paths (could be invalid)
-path_counter: number of paths
-*/
+#define MAX_PATHS 20
+#define MAX_COMMANDS 10
+#define ERROR_MESSAGE "An error has occurred\n"
+
 // Function to clear the PATH
-void clear_path(char *paths[], size_t *path_counter)
-{
-  int j;
-  for (j = 0; j < *path_counter; j++)
-  {
-    free(paths[j]); // Free each allocated path
-    paths[j] = NULL;
-  }
-  *path_counter = 0;
+void clearPath(char* paths[], size_t* path_counter) {
+    int j;
+    for (j = 0; j < *path_counter; j++) {
+        free(paths[j]);
+        paths[j] = NULL;
+    }
+    *path_counter = 0;
 }
-/*
-paths: all the potential paths (could be invalid)
-path_counter: number of paths
-*/
+
 // Function to find the executable in the provided paths
-char *find_executable(char *command, char *paths[], size_t *path_counter)
-{
-  char full_path[MAX_PATH_LENGTH];
-  int i = 0;
-  for (; i < *path_counter; i++)
-  {
-    snprintf(full_path, sizeof(full_path), "%s/%s", paths[i], command); // Construct the full path
-    if (access(full_path, X_OK) == 0)
-    {                           // Check if the command is executable
-      return strdup(full_path); // Return a copy of the full path
-    }
-  }
-  return NULL; // Return NULL if not found
-}
-/*
-args: Stack allocated arguments
-paths: all the potential paths (could be invalid)
-path_counter: number of paths
-redirection: flag to check if redirect
-output_file: file descriptor to redirect
-*/
-void execute_command(char **args, char *paths[], size_t *path_counter, bool redirection, char *output_file)
-{
-  char *executable = find_executable(args[0], paths, path_counter);
-  if (executable == NULL)
-  {
-    write(STDERR_FILENO, error_message, strlen(error_message));
-    return;
-  }
-  pid_t rc = fork();
-  if (rc < 0)
-  {
-    write(STDERR_FILENO, error_message, strlen(error_message));
-    return;
-  }
-  else if (rc == 0)
-  {
-    if (redirection)
-    {
-      int fd = open(output_file, O_WRONLY | O_TRUNC | O_CREAT, S_IRWXU);
-      if (fd < 0)
-      {
-        write(STDERR_FILENO, error_message, strlen(error_message));
-        return;
-      }
-      // Assign fd to stdout and stderr, so all messages will be redirected to the file,
-      dup2(fd, STDOUT_FILENO);
-      dup2(fd, STDERR_FILENO);
-      close(fd); // Close the original fd
-    }
-    execv(executable, args);
-    write(STDERR_FILENO, error_message, strlen(error_message));
-    exit(1);
-  }
-  free(executable);
-}
-void builtin(char **args, int args_count, char *paths[], size_t *path_counter)
-{
-  if (strcmp(args[0], "exit") == 0)
-  {
-    if (args_count > 1)
-    {
-      write(STDERR_FILENO, error_message, strlen(error_message));
-      return;
-    }
-    exit(0);
-  }
-  else if (strcmp(args[0], "cd") == 0)
-  {
-    if (args_count != 2)
-    {
-      write(STDERR_FILENO, error_message, strlen(error_message));
-      return;
-    }
-    else if (chdir(args[1]) != 0) // Error in changing directory
-    {
-      write(STDERR_FILENO, error_message, strlen(error_message));
-    }
-  }
-  else if (strcmp(args[0], "path") == 0)
-  {
-    clear_path(paths, path_counter);
+char* find_executable(char* command, char* paths[], size_t counter) {
+    char full_path[100];
     int i;
-    for (i = 1; i < args_count; i++)
-    {
-      paths[(*path_counter)] = strdup(args[i]); // Don't check, that is for find_executable
-      (*path_counter)++;
-    }
-  }
-}
-/*
-string: Entire Line
-paths: all the potential paths (could be invalid)
-path_counter: number of paths
-
-*/
-void process_line(char *string, char *paths[], size_t *path_counter)
-{
-  char *commands[MAX_COMMAND];
-  int command_count = 0;
-  char *command_token;
-  char *saveptr1;
-  // Check to see if there are multiple commands
-
-  command_token = strtok_r(string, "&", &saveptr1);
-  while (command_token != NULL && command_count < MAX_COMMAND)
-  {
-    commands[command_count++] = command_token;
-    command_token = strtok_r(NULL, "&", &saveptr1);
-  }
-  // For every command, we execute them
-  int cmd = 0;
-  for (; cmd < command_count; cmd++)
-  {
-    int args_count = 0;
-    char *args[MAX_ARGS]; // command  + arguments
-    char *args_token;
-    char *saveptr2;
-    bool redirection = false; // If redirection
-    char *output_file = NULL;
-
-    args_token = strtok_r(commands[cmd], " \t\n", &saveptr2);
-    while (args_token != NULL && args_count < MAX_ARGS - 1)
-    {
-      if (strcmp(args_token, ">") == 0) // In this very command, there is a redirection
-      {
-        // Need to check if there is another argument to serve as the output file
-        redirection = true;
-        args_token = strtok_r(NULL, " \t\n", &saveptr2);
-        // If there's a file path
-        if (args_token != NULL)
-        {
-          output_file = args_token;
-          args_token = strtok_r(NULL, " \t\n", &saveptr2);
-          if (args_token != NULL) // Additional Path??
-          {
-            write(STDERR_FILENO, error_message, strlen(error_message));
-            return;
-          }
+    for (i = 0; i < counter; i++) {
+        snprintf(full_path, sizeof(full_path), "%s/%s", paths[i], command);
+        if (access(full_path, X_OK) == 0) {
+            return strdup(full_path);
         }
-        else
-        {
-          write(STDERR_FILENO, error_message, strlen(error_message));
-          return;
-        }
-        // If redirection, then we are done with this command
-        break;
-      }
-      //
-      args[args_count++] = args_token;
-      args_token = strtok_r(NULL, " \t\n", &saveptr2);
     }
-
-    args[args_count] = NULL;
-    // If there's zero arg, just go to the next round.
-    if (args_count == 0)
-      continue;
-    if (strcmp(args[0], "exit") == 0 || strcmp(args[0], "cd") == 0 || strcmp(args[0], "path") == 0)
-    {
-      builtin(args, args_count, paths, path_counter);
-    }
-    else
-    {
-      execute_command(args, paths, path_counter, redirection, output_file); // Output file is NULL by default, if there's output file, will be changed in >
-    }
-  }
-  // Waiting for all the children
-  int i;
-  for (i = 0; i < command_count; i++)
-  {
-    wait(NULL);
-  }
+    return NULL;
 }
 
-int main(int argc, char *argv[])
-{
-  char *paths[MAX_PATH] = {strdup("/bin")}; // Initialize with /bin
-  size_t path_counter = 1;                  // Path count
-
-  if (argc > 2)
-  {
-    write(STDERR_FILENO, error_message, strlen(error_message));
-    exit(1);
-  }
-  else if (argc == 2) // This is batch mode
-  {
-    FILE *batch = fopen(argv[1], "r");
-    if (batch == NULL)
-    {
-      write(STDERR_FILENO, error_message, strlen(error_message));
-      exit(1);
+// Function to execute a single command
+void execute_command(char** args, char* paths[], size_t path_counter, bool redirection, char* output_file) {
+    char* executable = find_executable(args[0], paths, path_counter);
+    if (executable == NULL) {
+        write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+        return;
     }
-    char *string = NULL;
-    size_t len = 0;
-    ssize_t read;
 
-    while ((read = getline(&string, &len, batch)) != -1) // For every line, treat it as an keyboard input + ENTER
-    {
-      process_line(string, paths, &path_counter);
-    }
-    free(string);
-    fclose(batch);
-  }
-  else
-  {
-    while (1)
-    {
-      printf("dash >");
-      fflush(stdout);
-      char *string = NULL;
-      size_t len = 0;
-      ssize_t read;
-
-      if ((read = getline(&string, &len, stdin)) == -1)
-      {
-        if (feof(stdin))
-        {
-          exit(0);
-        }
-
-        write(STDERR_FILENO, error_message, strlen(error_message));
+    int rc = fork();
+    if (rc < 0) {
+        write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
         exit(1);
-      }
-      process_line(string, paths, &path_counter, false);
-      free(string);
+    } else if (rc == 0) {  // Child process
+        if (redirection) {
+            int fd = open(output_file, O_WRONLY|O_TRUNC|O_CREAT, S_IRWXU);
+            if (fd < 0) {
+                write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+                exit(1);
+            }
+            dup2(fd, STDOUT_FILENO);
+            dup2(fd, STDERR_FILENO);
+            close(fd);
+        }
+        execv(executable, args);
+        write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+        exit(1);
     }
-  }
-  clear_path(paths, &path_counter);
-  return 0;
+    free(executable);
+}
+
+// Function to process a line of input (either from interactive mode or batch file)
+void process_line(char* line, char* paths[], size_t* path_counter, bool batch_mode) {
+    char* commands[MAX_COMMANDS];
+    int command_count = 0;
+    char* command_token;
+    char* saveptr1;
+
+    // Split the line into multiple commands (if any)
+    command_token = strtok_r(line, "&", &saveptr1);
+    while (command_token != NULL && command_count < MAX_COMMANDS) {
+        commands[command_count++] = command_token;
+        command_token = strtok_r(NULL, "&", &saveptr1);
+    }
+
+    int cmd;
+    for (cmd = 0; cmd < command_count; cmd++) {
+        char* args[20];
+        int arg_count = 0;
+        char* arg_token;
+        char* saveptr2;
+        bool redirection = false;
+        char* output_file = NULL;
+
+        // Parse the command and its arguments
+        arg_token = strtok_r(commands[cmd], " \t\n", &saveptr2);
+        while (arg_token != NULL && arg_count < 19) {
+            if (strcmp(arg_token, ">") == 0) {
+                redirection = true;
+                arg_token = strtok_r(NULL, " \t\n", &saveptr2);
+                if (arg_token != NULL) {
+                    output_file = arg_token;
+                } else {
+                    write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+                    return;
+                }
+                break;
+            }
+            args[arg_count++] = arg_token;
+            arg_token = strtok_r(NULL, " \t\n", &saveptr2);
+        }
+        args[arg_count] = NULL;
+
+        if (arg_count == 0) continue;  // Skip empty commands
+
+        // Handle built-in commands
+        if (strcmp(args[0], "exit") == 0) {
+            if (arg_count > 1 || batch_mode) {
+                write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+                if (!batch_mode) continue;
+            }
+            exit(0);
+        } else if (strcmp(args[0], "cd") == 0) {
+            if (arg_count != 2) {
+                write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+            } else if (chdir(args[1]) != 0) {
+                write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+            }
+        } else if (strcmp(args[0], "path") == 0) {
+            clearPath(paths, path_counter);
+            int i;
+            for (i = 1; i < arg_count; i++) {
+                paths[*path_counter] = strdup(args[i]);
+                (*path_counter)++;
+            }
+        } else {
+            execute_command(args, paths, *path_counter, redirection, output_file);
+        }
+    }
+
+    // Wait for all child processes to complete
+    while (wait(NULL) > 0);
+}
+
+int main(int argc, char* argv[]) {
+    char* paths[MAX_PATHS] = { strdup("/bin"), NULL };
+    size_t path_counter = 1;
+
+    if (argc > 2) {
+        write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+        exit(1);
+    }
+
+    if (argc == 2) {
+        // Batch mode
+        FILE* batch_file = fopen(argv[1], "r");
+        if (batch_file == NULL) {
+            write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+            exit(1);
+        }
+
+        char* line = NULL;
+        size_t len = 0;
+        ssize_t read;
+
+        while ((read = getline(&line, &len, batch_file)) != -1) {
+            process_line(line, paths, &path_counter, true);
+        }
+
+        free(line);
+        fclose(batch_file);
+    } else {
+        // Interactive mode
+        while (1) {
+            printf("dash> ");
+            char* line = NULL;
+            size_t len = 0;
+            ssize_t read = getline(&line, &len, stdin);
+
+            if (read == -1) {
+                clearPath(paths, &path_counter);
+                exit(0);
+            }
+
+            process_line(line, paths, &path_counter, false);
+            free(line);
+        }
+    }
+
+    clearPath(paths, &path_counter);
+    return 0;
 }
